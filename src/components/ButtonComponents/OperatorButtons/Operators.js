af2c64c4370d137f47de9f868ddd059e7017b1d4
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-16 13:27:40 -0700

    fix import errors

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import OperatorButton from './OperatorButton.js'
 
 //Import your array data to from the provided data file
-import { operators } from '../../../data.js'
+import operators from '../../../data.js'
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
