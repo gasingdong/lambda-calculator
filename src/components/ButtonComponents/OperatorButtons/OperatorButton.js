96cb612406dbe4b012251a16023c89ae581fa5e4
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-16 13:47:17 -0700

    render buttons to app

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({char, value}) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+       <button>{char}</button>
+      }
     </>
   );
 };
