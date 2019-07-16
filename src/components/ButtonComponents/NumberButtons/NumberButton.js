80cb83ffdec64d4933cd8dd7ce35cffbbbd16b9e
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-16 15:19:35 -0700

    finish styling

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const NumberButton = ({number}) => {
+  console.log(number);
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */
-       <button>{number}</button>
+       <button className={number === '0' ? "zero" : "regularNumber"}>{number}</button>
       }
     </>
   );
