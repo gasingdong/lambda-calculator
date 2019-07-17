0461940ad77d4a290ec458593267694e8c42324c
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-16 19:45:25 -0700

    all operators now functional

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,10 +3,14 @@ import React from "react";
 function calculate(operator, total, next) {
   total = parseFloat(total);
   next = parseFloat(next);
-  if (operator.operator.value === '+') {
-    total += next;
+  switch (operator.operator.value) {
+    case '+': total += next; break;
+    case '-': total -= next; break;
+    case '*': total *= next; break;
+    case '/': total /= next; break;
+    default: break;
   }
-  return "" + total;
+  return "" + total.toPrecision(12).replace(/.?0+$/,"");
 }
 
 const OperatorButton = ({operator, state, setState}) => {
@@ -15,6 +19,14 @@ const OperatorButton = ({operator, state, setState}) => {
       {/* Display a button element rendering the data being passed down from the parent container on props */
        <button onClick={() => {
          if (operator.value === '=') {
+
+           if (state.next === '0') {
+             return setState({
+               total: state.total,
+               operator: {operator},
+               next: '0',
+             })
+           }
            return setState({
              total: calculate(state.operator, state.total, state.next),
              operator: {operator},
